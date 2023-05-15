902127d8df9d930ead84f89064aeebe02afa14f9
Morgan Dilling <morgandilling171@gmail.com>
2023-05-15 12:53:43 +0100

    feat: Command handling

---
+++ src/events/InteractionCreate.ts
@@ -0,0 +1,33 @@
+import DiscordEvent from 'classes/Event';
+import ExtendedClient from 'classes/ExtendedClient';
+import { Interaction } from 'discord.js';
+
+export default class InteractionCreate extends DiscordEvent {
+  public static once = false;
+
+  public static execute(
+    client: ExtendedClient,
+    interaction: Interaction
+  ): void {
+    if (interaction.isCommand()) {
+      const command = client.commands.get(interaction.commandName);
+
+      if (!command) return;
+
+      client.logger.info(
+        `Command ${interaction.commandName} was called by ${interaction.user.tag}`
+      );
+
+      try {
+        command.execute(client, interaction);
+      } catch (error) {
+        client.logger.error(error);
+        interaction.reply({
+          content:
+            '> :warning: | An error occured while executing this command',
+          ephemeral: true,
+        });
+      }
+    }
+  }
+}
